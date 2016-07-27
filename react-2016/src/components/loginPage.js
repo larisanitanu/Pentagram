b2ee8645c2193ae4e2799bcce51c291ea0e487d6
larisanitanu <larisanitanu@gmail.com>
2016-07-27 13:52:24 +0300

    Registration/Login

--- react-2016/src/components/loginPage.js
+++ react-2016/src/components/loginPage.js
@@ -6,25 +6,53 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 var Login = React.createClass({
-    getInitialState: function() {
-    return {value: 'Username'};
+    getInitialState: function () {
+        return {
+            username: null
+            , password: null
+        };
     },
-    handleChange: function (e) {
-        this.setState({value: e.target.value});
+    userChangeHandler: function (e) {
+        this.setState({username: e.target.value});
 
     },
-    render: function(){
-        
+    PasswordChangeHandler: function (e) {
+        this.setState({password: e.target.value});
+
+    },
+    formSubmitHandler: function (e) {
+        e.preventDefault();
+        console.log(this.state);
+        $.ajax({
+            url: 'http://127.0.0.1:8000/api/v1/login/'
+            , type: 'POST'
+            , data: this.state
+            , error: function (response) {
+                    console.log(response.responseJSON.non_field_errors[0]);
+            }
+            ,success: function() {
+                console.log("User logged in successfully completed!");
+            }
+        }).then(function (data) {
+            sessionStorage.setItem('authToken', data.token);
+            //redir homepage
+        });
+
+    },
+    render: function () {
+
         return (
             <div>
                 <div className="text-center jumbotron">
                     <image src="image/logo.png" width="200px"/>
                     <form>
                         <br />
-                        <p><input type="text" name="username" label="" onChange={this.props.onChange} placeholder="Username"/></p>
-                        <p><input type="password" name="password" label="" onCange={this.props.onChange} placeholder="Password"/> </p>
+                        <p><input type="text" name="username" label="" onChange={this.userChangeHandler}
+                                  placeholder="Username"/></p>
+                        <p><input type="password" name="password" label="" onChange={this.PasswordChangeHandler}
+                                  placeholder="Password"/></p>
                         <br />
-                        <button type="submit">Login</button>
+                        <button className="button" type="submit" onClick={this.formSubmitHandler}>Login</button>
                     </form>
                 </div>
                 <div className="text-center jumbotron">
--- react-2016/src/components/registerPage.js
+++ react-2016/src/components/registerPage.js
@@ -4,25 +4,72 @@ var React = require('react');
 var Input = require('./common/textInput');
 
 var Register = React.createClass({
-    getInitialState: function() {
-    return {value: 'Username'};
+    getInitialState: function () {
+        return {
+            username: null
+            , password: null
+            , passwordRepeat: null
+            , email: null
+        };
     },
-    handleChange: function (e) {
-        this.setState({value: e.target.value});
+    userChangeHandler: function (e) {
+        this.setState({username: e.target.value});
 
     },
-    render: function(){
+    passwordChangeHandler: function (e) {
+        this.setState({password: e.target.value});
+
+    },
+    passwordRepeatChangeHandler: function (e) {
+        this.setState({passwordRepeat: e.target.value});
+
+    },
+    emailChangeHandler: function (e) {
+        this.setState({email: e.target.value});
+
+    },
+    formSubmitHandler: function (e) {
+        e.preventDefault();
+        console.log(this.state);
+        if (this.state.password === this.state.passwordRepeat) {
+            $.ajax({
+                url: 'http://127.0.0.1:8000/api/v1/users/'
+                , type: 'POST'
+                , data: this.state
+                , success: function () {
+                    console.log("User registration successfully completed!");
+                }
+                , error: function (response) {
+                    console.log(response.responseJSON);
+                }
+            }).then(function (data) {
+                //sessionStorage.setItem('authToken', data.token);
+                //redir homepage
+            });
+        }
+        else {
+            console.log("Password doesn't match!");
+        }
+
+    },
+
+    render: function () {
         return (
             <div className="text-center jumbotron">
-                <image src="image/logo.png" width="200px" />
+                <image src="image/logo.png" width="200px"/>
                 <form>
                     <br />
-                    <p><input type="text" name="username" label="" onChange={this.props.onChange} placeholder="Username"/></p>
-                    <p><input type="text" name="username" label="" onChange={this.props.onChange} placeholder="Email"/></p>
-                    <p><input type="password" name="password" label="" onCange={this.props.onChange} placeholder="Password"/> </p>
-                    <p><input type="password" name="password" label="" onCange={this.props.onChange} placeholder="Retype password"/> </p>
+                    <p><input type="text" name="username" label="" onChange={this.userChangeHandler}
+                              placeholder="Username"/></p>
+                    <p><input type="text" name="email" label="" onChange={this.emailChangeHandler} placeholder="Email"/>
+                    </p>
+                    <p><input type="password" name="password" label="" onChange={this.passwordChangeHandler}
+                              placeholder="Password" required/></p>
+                    <p><input type="password" name="passwordRepeat" label=""
+                              onChange={this.passwordRepeatChangeHandler}
+                              placeholder="Confirm password" required/></p>
                     <br />
-                    <button type="submit">Sign up</button>
+                    <button type="submit" className="button" onClick={this.formSubmitHandler}>Sign up</button>
                 </form>
             </div>
         );
